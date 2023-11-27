ab6cbbc3e49534b185e3486c9e4d1bd2e7145a19
Felipe de Jesus <50275021+Felipedejesuslopez@users.noreply.github.com>
2023-11-26 21:54:38 -0600

    Commit 2 26 nov 2023

--- login/index.php
+++ login/index.php
@@ -11,13 +11,12 @@
 </head>
 
 <body>
-
-    <br><br><br>
+<br><br><br>
     <div class="container">
         <div class="row">
-            <div class="col-md-4 login-sec">
+            <div class="col-md-12 col-lg-4 login-sec">
                 <center>
-                    <img src="../img/JobsOnLight.png" class="logo">
+                    <img src="../img/JobsOn.png" class="logo">
                 </center>
                 <h2 class="text-center">Inicie Sesión</h2>
                 <div id="res"></div>
@@ -25,14 +24,11 @@
                     <div class="form-group">
                         <label for="exampleInputEmail1" class="text-uppercase">Usuario/Email</label>
                         <input type="text" name="user" class="form-control">
-
                     </div>
                     <div class="form-group">
                         <label class="text-uppercase">Contraseña</label>
                         <input type="password" class="form-control" name="psw">
                     </div>
-
-
                     <div class="form-check">
                         <label class="form-check-label">
                             <input type="checkbox" class="form-check-input" name="noclose">
@@ -40,55 +36,22 @@
                         </label>
                         <button type="submit" class="btn btn-login float-right">Iniciar Sesión</button>
                     </div>
-
                 </form>
             </div>
-            <div class="col-md-8 banner-sec">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner" role="listbox">
-                        <div class="carousel-item active">
-                            <img class="d-block img-fluid" src="https://static.pexels.com/photos/33972/pexels-photo.jpg" alt="First slide">
-                            <div class="carousel-caption d-none d-md-block">
-                                <div class="banner-text">
-                                    <h2>This is Heaven</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block img-fluid" src="https://images.pexels.com/photos/7097/people-coffee-tea-meeting.jpg" alt="First slide">
-                            <div class="carousel-caption d-none d-md-block">
-                                <div class="banner-text">
-                                    <h2>This is Heaven</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block img-fluid" src="../img/JobsOnDark.png" alt="First slide">
-                            <div class="carousel-caption d-none d-md-block">
-                                <div class="banner-text">
-                                    <h2>This is Heaven</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
+            <div class="col-md-12 col-lg-8 banner-sec">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <!-- El resto de tu código del carrusel -->
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="container2" style="text-align: right;">
-            <br>
-            ¿No tienes una cuenta?
-            <a href="../Postulantes/Registro/">Regístrate</a> ahora
-        </div>
+    <div class="container2" style="text-align: center;">
+        <br>
+        ¿No tienes una cuenta?
+        <a href="../Postulantes/Registro/">Regístrate</a> ahora
+    </div>
 </body>
 
 </html>
