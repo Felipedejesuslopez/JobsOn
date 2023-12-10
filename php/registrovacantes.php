5a185a6c1d967b258cda2006e0569f8b0a75378f
JRosales724 <115046789+JRosales724@users.noreply.github.com>
2023-12-10 17:16:51 -0600

    Add files via upload

--- php/registrovacantes.php
+++ php/registrovacantes.php
@@ -5,6 +5,31 @@ require_once '../clases/class.conexion.php';
 require_once '../clases/class.ofertalaboral.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $fechaPublicacion = date('Y-m-d');
+    $usuarioID = obtenerUsuarioID();
+
+    $directorioImagenes = "../imagenes_vacantes/{$usuarioID}/";
+
+    if (!file_exists($directorioImagenes)) {
+        mkdir($directorioImagenes, 0777, true);
+    }
+
+    $imagen1Tmp = $_FILES['imagen1']['tmp_name'];
+    $imagen2Tmp = $_FILES['imagen2']['tmp_name'];
+    $imagen3Tmp = $_FILES['imagen3']['tmp_name'];
+
+    $nombreImagen1 = $_FILES['imagen1']['name'];
+    $nombreImagen2 = $_FILES['imagen2']['name'];
+    $nombreImagen3 = $_FILES['imagen3']['name'];
+
+    move_uploaded_file($imagen1Tmp, $directorioImagenes . $nombreImagen1);
+    move_uploaded_file($imagen2Tmp, $directorioImagenes . $nombreImagen2);
+    move_uploaded_file($imagen3Tmp, $directorioImagenes . $nombreImagen3);
+
+    $rutaImagen1 = $directorioImagenes . $nombreImagen1;
+    $rutaImagen2 = $directorioImagenes . $nombreImagen2;
+    $rutaImagen3 = $directorioImagenes . $nombreImagen3;
+
     $oferta = new OfertaLaboral(
         null,
         $_POST['titulo'],
@@ -12,12 +37,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $_POST['ubicacion'],
         $_POST['salario'],
         $_POST['requisitos'],
-        $_POST['fechaPublicacion'],
+        $fechaPublicacion,
         $_POST['fechaExpiracion'],
         $_POST['empresa'],
         $_POST['tipoContrato'],
         $_POST['nivelExperiencia'],
-        $_POST['beneficios']
+        $_POST['beneficios'],
+        $rutaImagen1,
+        $rutaImagen2,
+        $rutaImagen3
     );
 
     $resultado = $oferta->create();
@@ -26,6 +54,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "<p style='color: green;'>Oferta registrada con éxito.</p>";
     } else {
         echo "<p style='color: red;'>Error al registrar la oferta.</p>";
+    }
+}
+
+function obtenerUsuarioID() {
+    session_start();
+
+    if (isset($_SESSION['usuario_id'])) {
+        return $_SESSION['usuario_id'];
+    } else {
+        return 1;
     }
 }
 ?>
