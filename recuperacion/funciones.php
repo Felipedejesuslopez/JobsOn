8f34cbde28061be37ac92ffb52839e9492183181
JRosales724 <115046789+JRosales724@users.noreply.github.com>
2023-12-11 03:48:34 -0600

    Add files via upload

--- recuperacion/funciones.php
+++ recuperacion/funciones.php
@@ -7,42 +7,37 @@ function generateToken() {
 
 function emailExists($email) {
     $conexion = new Conexion();
-    
+
     $email = $conexion->real_escape_string($email);
 
-    // Verifica la existencia del correo electrónico
     $sql = "SELECT * FROM usuarios WHERE email = '$email'";
     $resultado = $conexion->query($sql);
 
     if ($resultado->num_rows > 0) {
         $conexion->close();
-        return true; // El correo electrónico existe
+        return true; 
     } else {
         $conexion->close();
-        return false; // El correo electrónico no existe
+        return false;
     }
 }
 
 function changePassword($email, $newPassword) {
     $conexion = new Conexion();
 
-    // Escapa el correo electrónico para evitar inyección de SQL
     $email = $conexion->real_escape_string($email);
 
-    // Hash de la nueva contraseña
     $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
 
-    // Consulta SQL para actualizar la contraseña
     $sql = "UPDATE usuarios SET password = '$hashedPassword' WHERE email = '$email'";
     $resultado = $conexion->query($sql);
 
-    // Verifica si la consulta fue exitosa
     if ($resultado) {
         $conexion->close();
-        return true; // Contraseña cambiada exitosamente
+        return true;
     } else {
         $conexion->close();
-        return false; // Error al cambiar la contraseña
+        return false;
     }
 }
 ?>
