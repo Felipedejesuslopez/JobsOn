0fbd279ab465ba1778b63ba25fb5027bb95c7ba4
Felipe de Jesus <50275021+Felipedejesuslopez@users.noreply.github.com>
2023-12-11 15:21:26 -0600

    Add files via upload

--- vacantes/detalles/index.php
+++ vacantes/detalles/index.php
@@ -25,5 +25,31 @@ $empresa = $em->read()->fetch_array();
         <h1><?php echo $oferta['TITULO']; ?></h1>
     </center>
     <br>
-    <?php echo $empresa[''] ?>
+    <div class="row">
+        <div class="col-sm-1 col-2">
+            <img src="img/empresas/<?php echo $empresa['ID']; ?>.jpg" style="width:100%;">
+        </div>
+        <div class="col-sm-2 col-8" style="vertical-align:center;">
+            <b><?php echo $empresa['NOMBRE']; ?></b>
+        </div>
+        <div class="col-sm-7"></div>
+        <div class="col-sm-2 col-2" style="text-align:right;">
+            $<?php echo $oferta['SALARIO']; ?>
+        </div>
+    </div>
+    <p style="text-align:justify; font-size:10pt;"><?php echo utf8_encode($oferta['DESCRIPCION']); ?></p>
+    <div class="row">
+        <div class="col-sm-6">
+            <b>Requisitos:</b>
+            <p style="text-align:justify; font-size:10pt;"><?php echo utf8_encode($oferta['REQUISITOS']); ?></p>
+        </div>
+        <div class="col-sm-6">
+            <b>Beneficios:</b>
+            <p style="text-align:justify; font-size:10pt;"><?php echo utf8_encode($oferta['BENEFICIOS']); ?></p><br>
+            <b>Nivel de Experiencia:</b>
+            <p style="text-align:justify; font-size:10pt;"><?php echo utf8_encode($oferta['NIVEL_EXPERIENCIA']); ?></p>
+        </div>
+    </div>
+
+    <button class="btn btn-primary" style="width:100%;">Postularme</button>
 </div>
--- vacantes/registro/index.php
+++ vacantes/registro/index.php
@@ -1,60 +1,52 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de Oferta Laboral</title>
-</head>
-<body>
 
-<div class="container">
-    <h2>Registro de Oferta Laboral</h2>
-    <form action="php/registrovacantes.php" method="post" enctype="multipart/form-data">
-        
-        <label for="titulo">Título:</label>
-        <input type="text" id="titulo" name="titulo" required>
 
-        <label for="descripcion">Descripción:</label>
-        <textarea id="descripcion" name="descripcion" rows="4" required></textarea>
+    <div class="container">
+        <h2>Registro de Oferta Laboral</h2>
+        <form action="php/registrovacantes.php" method="post" enctype="multipart/form-data">
+            <div form-group>
+                <label for="titulo">Título:</label>
+                <input type="text" id="titulo" name="titulo" class="form-control" required>
+            </div>
 
-        <label for="ubicacion">Ubicación:</label>
-        <input type="text" id="ubicacion" name="ubicacion" required>
+            <label for="descripcion">Descripción:</label>
+            <textarea id="descripcion" name="descripcion" rows="4" required></textarea>
 
-        <label for="salario">Salario:</label>
-        <input type="text" id="salario" name="salario" required>
+            <label for="ubicacion">Ubicación:</label>
+            <input type="text" id="ubicacion" name="ubicacion" required>
 
-        <label for="requisitos">Requisitos:</label>
-        <textarea id="requisitos" name="requisitos" rows="4" required></textarea>
+            <label for="salario">Salario:</label>
+            <input type="text" id="salario" name="salario" required>
 
-        <!-- Elimine el campo de fechaPublicacion para obtenerla del servidor -->
+            <label for="requisitos">Requisitos:</label>
+            <textarea id="requisitos" name="requisitos" rows="4" required></textarea>
 
-        <label for="fechaExpiracion">Fecha de Expiración:</label>
-        <input type="date" id="fechaExpiracion" name="fechaExpiracion" required>
+            <!-- Elimine el campo de fechaPublicacion para obtenerla del servidor -->
 
-        <label for="empresa">Empresa:</label>
-        <input type="text" id="empresa" name="empresa" required>
+            <label for="fechaExpiracion">Fecha de Expiración (opcional):</label>
+            <input type="date" id="fechaExpiracion" name="fechaExpiracion">
 
-        <label for="tipoContrato">Tipo de Contrato:</label>
-        <input type="text" id="tipoContrato" name="tipoContrato" required>
+            <label for="empresa">Empresa:</label>
+            <input type="text" id="empresa" name="empresa" style="display: none;" value="<?php session_start();
+                                                                                            echo $_SESSION['ID'] ?>">
 
-        <label for="nivelExperiencia">Nivel de Experiencia:</label>
-        <input type="text" id="nivelExperiencia" name="nivelExperiencia" required>
+            <label for="tipoContrato">Tipo de Contrato:</label>
+            <input type="text" id="tipoContrato" name="tipoContrato" required>
 
-        <label for="beneficios">Beneficios:</label>
-        <textarea id="beneficios" name="beneficios" rows="4" required></textarea>
+            <label for="nivelExperiencia">Nivel de Experiencia:</label>
+            <input type="text" id="nivelExperiencia" name="nivelExperiencia" required>
 
-        <label for="imagen1">Imagen 1:</label>
-        <input type="file" id="imagen1" name="imagen1" accept="image/*" required><br>
+            <label for="beneficios">Beneficios:</label>
+            <textarea id="beneficios" name="beneficios" rows="4" required></textarea>
 
-        <label for="imagen2">Imagen 2:</label>
-        <input type="file" id="imagen2" name="imagen2" accept="image/*" required><br>
+            <label for="imagen1">Imagen 1:</label>
+            <input type="file" id="imagen1" name="imagen1" accept="image/*" required><br>
 
-        <label for="imagen3">Imagen 3:</label>
-        <input type="file" id="imagen3" name="imagen3" accept="image/*" required><br>
+            <label for="imagen2">Imagen 2:</label>
+            <input type="file" id="imagen2" name="imagen2" accept="image/*" required><br>
 
-        <button type="submit">Registrar Oferta Laboral</button>
-    </form>
-</div>
+            <label for="imagen3">Imagen 3:</label>
+            <input type="file" id="imagen3" name="imagen3" accept="image/*" required><br>
 
-</body>
-</html>
+            <button type="submit">Registrar Oferta Laboral</button>
+        </form>
+    </div>
