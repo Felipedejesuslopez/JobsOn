626a75108a3e32af85aa05430a688af18f7c597d
Felipe de Jesus <50275021+Felipedejesuslopez@users.noreply.github.com>
2024-02-19 18:53:07 -0600

    Add files via upload

---
+++ resources/const.php
@@ -0,0 +1,13 @@
+<?php
+$l = [
+    'noobservaciones' => "No se redactaron observaciones",
+    'meet_summary' => "Reunión sw Google Meet para entrevista",
+    'meet_description' => "Reunión para la entrevista laboral de la vacante",
+    'message_agendado_1' => 'Buenas tardes, se le ha agendado una entrevista para el día ',
+    'message_agendado_2' => ' a las ',
+    'message_agendado_3' => ' a través del siguiente enlace:<br><br><center>',
+    'message_agendado_4' => '</center><br><br> Te pido de la manera más atenta posible que seas puntual, ya que contamos unicamente con 30 minutos de tiempo para llevar a cabo la entrevista',
+    'summary_meeting' => 'Entrevista para la vacante ',
+    'description_meeting' => 'Entrevista virtual para la vacante',
+    'at'=> 'a las',
+];
